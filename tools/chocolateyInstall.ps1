f0e8918f52ec4e5d6e0ea7ab7490f4eef64a9caa
Owain Perry <owain.perry@justgiving.com>
2014-04-16 13:43:54 +0100

    initial version

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'GoAgent'
+$silentArgs = '/S'
+$validExitCodes = @(0) 
+$url = "http://download01.thoughtworks.com/go/13.4.1/ga/go-agent-13.4.1-18342-setup.exe"
+$url64 = $url
+Install-ChocolateyPackage $packageName "exe" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
